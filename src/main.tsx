3a59006a27c3df627ccfb66abf518202ffbf6ba0
marcosmarinho <139090070+marcosfrancomarinho@users.noreply.github.com>
2026-08-13 21:16:02 -0300

    refactor: organiza ponto de entrada da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from "react-helmet-async";
-import App from './App.tsx';
+import { HelmetProvider } from 'react-helmet-async';
+
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
 
-<HelmetProvider>
-   <App />
-</HelmetProvider>
-)
+if (!rootElement) {
+  throw new Error('Elemento raiz não encontrado.');
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>,
+);
